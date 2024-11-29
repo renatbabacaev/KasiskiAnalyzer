4de421a5b0df5f45ddc18ddbcbc17d0c2062191d
Renat Babacaev <renat.babacaev@gmail.com>
2024-11-29 17:04:10 +0200

    Update Application Layout

--- KasiskiAnalyzer.Designer.cs
+++ KasiskiAnalyzer.Designer.cs
@@ -29,116 +29,133 @@
         private void InitializeComponent()
         {
             textCryptedText = new TextBox();
-            labelEnterCryptedText = new Label();
-            labelProcessedText = new Label();
             textProcessedText = new TextBox();
             buttonReset = new Button();
             buttonFindSequences = new Button();
             textKey = new TextBox();
-            labelEnterKey = new Label();
+            groupProcessed = new GroupBox();
+            groupCrypted = new GroupBox();
+            splitContainer1 = new SplitContainer();
+            groupProcessed.SuspendLayout();
+            groupCrypted.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // textCryptedText
             // 
-            textCryptedText.Location = new Point(10, 31);
+            textCryptedText.Location = new Point(6, 25);
             textCryptedText.Margin = new Padding(3, 2, 3, 2);
             textCryptedText.Multiline = true;
             textCryptedText.Name = "textCryptedText";
             textCryptedText.PlaceholderText = "Enter crypted text...";
             textCryptedText.ScrollBars = ScrollBars.Vertical;
-            textCryptedText.Size = new Size(291, 107);
+            textCryptedText.Size = new Size(429, 468);
             textCryptedText.TabIndex = 0;
-            // 
-            // labelEnterCryptedText
-            // 
-            labelEnterCryptedText.AutoSize = true;
-            labelEnterCryptedText.Location = new Point(10, 7);
-            labelEnterCryptedText.Name = "labelEnterCryptedText";
-            labelEnterCryptedText.Size = new Size(130, 20);
-            labelEnterCryptedText.TabIndex = 1;
-            labelEnterCryptedText.Text = "Enter Crypted Text";
-            // 
-            // labelProcessedText
-            // 
-            labelProcessedText.AutoSize = true;
-            labelProcessedText.Location = new Point(10, 140);
-            labelProcessedText.Name = "labelProcessedText";
-            labelProcessedText.Size = new Size(106, 20);
-            labelProcessedText.TabIndex = 2;
-            labelProcessedText.Text = "Processed Text";
             // 
             // textProcessedText
             // 
-            textProcessedText.Location = new Point(10, 162);
+            textProcessedText.Location = new Point(6, 25);
             textProcessedText.Margin = new Padding(3, 2, 3, 2);
             textProcessedText.Multiline = true;
             textProcessedText.Name = "textProcessedText";
             textProcessedText.PlaceholderText = "The result will be shown here...";
             textProcessedText.ReadOnly = true;
             textProcessedText.ScrollBars = ScrollBars.Vertical;
-            textProcessedText.Size = new Size(291, 93);
+            textProcessedText.Size = new Size(431, 492);
             textProcessedText.TabIndex = 3;
             // 
             // buttonReset
             // 
-            buttonReset.Location = new Point(318, 31);
+            buttonReset.Location = new Point(6, 527);
             buttonReset.Margin = new Padding(3, 2, 3, 2);
             buttonReset.Name = "buttonReset";
-            buttonReset.Size = new Size(134, 26);
+            buttonReset.Size = new Size(431, 77);
             buttonReset.TabIndex = 4;
             buttonReset.Text = "Reset";
             buttonReset.UseVisualStyleBackColor = true;
             // 
             // buttonFindSequences
             // 
-            buttonFindSequences.Location = new Point(318, 159);
+            buttonFindSequences.Location = new Point(6, 527);
             buttonFindSequences.Margin = new Padding(3, 2, 3, 2);
             buttonFindSequences.Name = "buttonFindSequences";
-            buttonFindSequences.Size = new Size(134, 96);
+            buttonFindSequences.Size = new Size(429, 77);
             buttonFindSequences.TabIndex = 5;
             buttonFindSequences.Text = "Find Sequences";
             buttonFindSequences.UseVisualStyleBackColor = true;
             // 
             // textKey
             // 
-            textKey.Location = new Point(511, 34);
+            textKey.Location = new Point(9, 500);
             textKey.Margin = new Padding(3, 2, 3, 2);
             textKey.Name = "textKey";
             textKey.PlaceholderText = "Enter Key...";
-            textKey.Size = new Size(110, 27);
+            textKey.Size = new Size(429, 27);
             textKey.TabIndex = 1;
             // 
-            // labelEnterKey
+            // groupProcessed
             // 
-            labelEnterKey.AutoSize = true;
-            labelEnterKey.Location = new Point(514, 10);
-            labelEnterKey.Name = "labelEnterKey";
-            labelEnterKey.Size = new Size(71, 20);
-            labelEnterKey.TabIndex = 7;
-            labelEnterKey.Text = "Enter Key";
+            groupProcessed.Controls.Add(buttonReset);
+            groupProcessed.Controls.Add(textProcessedText);
+            groupProcessed.Location = new Point(3, 3);
+            groupProcessed.Name = "groupProcessed";
+            groupProcessed.Size = new Size(443, 609);
+            groupProcessed.TabIndex = 8;
+            groupProcessed.TabStop = false;
+            groupProcessed.Text = "Processed Text";
+            // 
+            // groupCrypted
+            // 
+            groupCrypted.Controls.Add(buttonFindSequences);
+            groupCrypted.Controls.Add(textCryptedText);
+            groupCrypted.Location = new Point(3, 3);
+            groupCrypted.Name = "groupCrypted";
+            groupCrypted.Size = new Size(441, 612);
+            groupCrypted.TabIndex = 9;
+            groupCrypted.TabStop = false;
+            groupCrypted.Text = "Crypted Text";
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(12, 12);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(textKey);
+            splitContainer1.Panel1.Controls.Add(groupCrypted);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(groupProcessed);
+            splitContainer1.Size = new Size(900, 615);
+            splitContainer1.SplitterDistance = 446;
+            splitContainer1.TabIndex = 10;
             // 
             // KasiskiAnalyzer
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(628, 264);
-            Controls.Add(labelEnterKey);
-            Controls.Add(textKey);
-            Controls.Add(buttonFindSequences);
-            Controls.Add(buttonReset);
-            Controls.Add(textProcessedText);
-            Controls.Add(labelProcessedText);
-            Controls.Add(labelEnterCryptedText);
-            Controls.Add(textCryptedText);
+            ClientSize = new Size(924, 634);
+            Controls.Add(splitContainer1);
             Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             Margin = new Padding(3, 2, 3, 2);
-            MaximumSize = new Size(650, 315);
-            MinimumSize = new Size(650, 315);
             Name = "KasiskiAnalyzer";
             Text = "Kasiski Analyzer";
+            groupProcessed.ResumeLayout(false);
+            groupProcessed.PerformLayout();
+            groupCrypted.ResumeLayout(false);
+            groupCrypted.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -146,12 +163,12 @@
         private TextBox textCryptedText;
         private TextBox textProcessedText;
         private TextBox textKey;
-        private Label labelEnterCryptedText;
-        private Label labelProcessedText;
-        private Label labelEnterKey;
         private Button buttonReset;
         private Button buttonFindSequences;
 
         private TextHandler textHandler = new TextHandler();
+        private GroupBox groupProcessed;
+        private GroupBox groupCrypted;
+        private SplitContainer splitContainer1;
     }
 }
